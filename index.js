2be30480426ea581fc5cbf920607a921b15afa04
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-03-01 20:30:13 +0000

    add losing points refactor functions

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 var sort_step = (lst) => {
     for (var i = 0; i < lst.length - 1; i++) {
-        if (lst[i]<lst[i+1]){
+        if (lst[i] < lst[i + 1]) {
             tmp = lst[i];
-            lst[i] = lst[i+1];
-            lst[i+1] = tmp;
+            lst[i] = lst[i + 1];
+            lst[i + 1] = tmp;
             return lst;
         }
     }
     return lst;
 }
 var score = 0;
-
+var animating = false;
 $('#container').on('click', '.move', function () {
     const hilightclass = "hilight";
-    toggleClass($(this), hilightclass);
-    var selected = $(`#container .${hilightclass}`);
-    if (selected.length >= 2) {
-        let div1 = $(selected[0]);
-        let div2 = $(selected[1]);
-        const expected_order = sort_step(getCurrentValues()).join();
-        swap_divs(div1, div2, 2000, () => {
-            console.log(expected_order);
-            const current_order = getCurrentValues().join();
-            console.log(current_order);
-            if (expected_order == current_order)
-                score++;
-            $('#score').text(score);
-        });
-        div1.removeClass(hilightclass);
-        div2.removeClass(hilightclass);
+    if (!animating) {
+        toggleClass($(this), hilightclass);
+        var selected = $(`#container .${hilightclass}`);
+        if (selected.length >= 2) {
+            animating = true;
+            let div1 = $(selected[0]);
+            let div2 = $(selected[1]);
+            const expected_order = sort_step(getCurrentValues()).join();
+            div1.removeClass(hilightclass);
+            div2.removeClass(hilightclass);
+            swap_divs(div1, div2, 200, () => {
+                const current_order = getCurrentValues().join();
+                if (expected_order == current_order)
+                    score++;
+                else
+                    score--;
+                $('#score').text(score);
+                animating = false;
+            });
+        }
     }
 })
 
@@ -52,20 +56,22 @@ function swapDivInHeierachy(div1, div2) {
 function swap_divs(div1, div2, t, cb) {
     distancex = div1.offset().left - div2.offset().left;
     distancey = div1.offset().top - div2.offset().top;
+    const animate_to = (div, x, y, t) => div.animate({
+        left: x,
+        top: y
+    }, t);
+    const reset_animation = div => {
+        div.css('left', '0px');
+        div.css('top', '0px');
+    }
 
     if (div2.length) {
-        $.when(div1.animate({
-            left: -distancex,
-            top: -distancey
-        }, t),
-            div2.animate({
-                left: distancex,
-                top: distancey
-            }, t)).done(function () {
-                div2.css('left', '0px');
-                div1.css('left', '0px');
-                div2.css('top', '0px');
-                div1.css('top', '0px');
+        $.when(
+            animate_to(div1, -distancex, -distancey, t),
+            animate_to(div2, distancex, distancey, t))
+            .done(function () {
+                reset_animation(div1);
+                reset_animation(div2);
                 swapDivInHeierachy(div1, div2);
                 cb();
             });
