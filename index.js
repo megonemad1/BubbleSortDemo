91d9b5d667b772eed24d54d27b377708b946e229
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-03-02 22:19:22 +0000

    fix sort test, now actuly bubblesort added winstate

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-let sort_step = (lst) => {
-    for (let i = 0; i < lst.length - 1; i++) {
-        if (lst[i] < lst[i + 1]) {
-            tmp = lst[i];
-            lst[i] = lst[i + 1];
-            lst[i + 1] = tmp;
-            return lst;
+let sort_steps = (lst) => {
+    rval = []
+    done = false;
+    for (let x = 0; x < lst.length; x++) {
+        for (let i = 0; i < lst.length - 1; i++) {
+            if (lst[i] < lst[i + 1]) {
+                tmp = lst[i];
+                lst[i] = lst[i + 1];
+                lst[i + 1] = tmp;
+
+                var order = lst.join();
+                rval.push(order);
+            }
         }
+        if (rval.slice(-2)[0] == rval.slice(-1)[0])
+            break;
     }
-    return lst;
+    return rval;
 }
+
+
 const max_life = 3;
 let cLife = max_life;
 const life_container = $("#lifeBar");
-const make_life = node => {
-    for (let x = 0; x < max_life; x++) {
-        node.append(`<img id="life_${x}" class="life_container"/>`);
-    }
-    return life => {
-        node.children('').each(function (i, life_container) {
-            life_container.setAttribute("src", i < life ? "Images\\Full_Life.svg" : "Images\\Empty_Life.svg");
-        });
-    };
-};
-const set_life = make_life(life_container);
 const item_container = $("#container");
+const make_life = node => {
+    for (let x = 0; x < max_life; x++)
+        node.append(`<img id="life_${x}" class="life_container"/>`);
+
+    return life => node.children('').each(function (i, life_container) {
+        life_container.setAttribute("src", i < life ? "Images\\Full_Life.svg" : "Images\\Empty_Life.svg");
+    });
+};
 const item_gen = (node) => {
     return n => {
-        let count = node.children.length;
-        for (let x = 0; x < Math.abs(n); x++) {
+        let count = node.children().length;
+        for (let x = 0; x < Math.abs(n); x++)
             if (n >= 0)
                 node.append(`<div id="item_${count}" class="move">Item ${count++}</div>`);
             else
-                $(node).remove(`#item_${--count - x}`);
-        }
-
+                $(node).children(`#item_${--count - x}`).remove();
+        return node.children();
     };
 };
+const set_life = make_life(life_container);
 const item_factory = item_gen(item_container);
 item_factory(5);
 set_life(cLife);
+var steps = sort_steps(getCurrentValues());
 let animating = false;
 $('#container').on('click', '.move', function () {
     const hilightclass = "hilight";
@@ -49,16 +57,25 @@ $('#container').on('click', '.move', function () {
             animating = true;
             let div1 = $(selected[0]);
             let div2 = $(selected[1]);
-            const expected_order = sort_step(getCurrentValues()).join();
             div1.removeClass(hilightclass);
             div2.removeClass(hilightclass);
-            swap_divs(div1, div2, 200, () => {
-                const current = getCurrentValues();
-                current_order = current.join();
-                next_step = sort_step(current).join();
-                console.log(`${expected_order} == ${current_order}`);
-                if (expected_order != current_order)
+            swap_divs(div1, div2, 200, (divs) => {
+                current_order = getCurrentValues().join();
+                if (steps[0] != current_order) {
                     cLife--;
+                    const errClass = 'err';
+                    divs.forEach(e => e.addClass(errClass));
+                    swap_divs(divs[0], divs[1], 200, (divs) => {
+                        divs.forEach(e => e.removeClass(errClass));
+                    });
+                }
+                else {
+                    steps.shift();
+                    if (steps.length <= 0){
+                        $('#container .move').addClass("win");
+                        $('#container .move').removeClass("move");
+                    }
+                }
                 set_life(cLife);
                 animating = false;
             });
@@ -77,11 +94,11 @@ function toggleClass(node, c) {
         node.addClass(c);
 }
 function swapDivInHeierachy(div1, div2) {
-
     tdiv1 = div1.clone();
     tdiv2 = div2.clone();
     div1.replaceWith(tdiv2);
     div2.replaceWith(tdiv1);
+    return [tdiv1, tdiv2];
 }
 function swap_divs(div1, div2, t, cb) {
     distancex = div1.offset().left - div2.offset().left;
@@ -91,8 +108,7 @@ function swap_divs(div1, div2, t, cb) {
         top: y
     }, t);
     const reset_animation = div => {
-        div.css('left', '0px');
-        div.css('top', '0px');
+        div.removeAttr('style');
     }
 
     if (div2.length) {
@@ -102,8 +118,7 @@ function swap_divs(div1, div2, t, cb) {
             .done(function () {
                 reset_animation(div1);
                 reset_animation(div2);
-                swapDivInHeierachy(div1, div2);
-                cb();
+                cb(swapDivInHeierachy(div1, div2));
             });
 
     }
