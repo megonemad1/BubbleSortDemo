56a96888b285450eccfc3fbb33d23ac07e30e5da
rhys puddephatt <rhys.puddephatt@btinternet.com>
2020-03-03 02:45:21 +0000

    unify attr usage

--- htmlTemplate.js
+++ htmlTemplate.js
@@ -1,5 +1,5 @@
 const script = $(document.currentScript);
-const template = script.attr('href');
+const template = script.attr('data');
 $.get(template, function(data) {
     script.replaceWith($(data).attr("data",template));
 });
